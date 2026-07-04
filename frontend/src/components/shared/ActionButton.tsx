8fe4e0985686995dbe55dbbf7225e2d4279fe669
Tamara Yustian <tamarayustian@gmail.com>
2026-07-04 22:22:16 +0800

    fix: add more props to accommodate more use cases

--- frontend/src/components/shared/ActionButton.tsx
+++ frontend/src/components/shared/ActionButton.tsx
@@ -4,6 +4,9 @@ import type { ReactNode } from "react";
 interface Props {
   href: string;
   bg?: string;
+  textColor?: string;
+  innerBg?: string;
+  innerHover?: string;
   hover?: string;
   iconHover?: string;
   className?: string;
@@ -13,19 +16,24 @@ interface Props {
 export function ActionButton({
   href,
   bg = "bg-gradient-accent",
+  textColor = "text-white",
+  innerBg = "",
+  innerHover = "hover:bg-white",
   hover = "group-hover:text-gradient-accent",
   iconHover = "group-hover:text-accent-red",
   className = "",
   children,
-}: Props) {
+}: Readonly<Props>) {
   return (
     <a
       href={href}
       target="_blank"
       rel="noopener noreferrer"
-      className={`mx-auto w-fit rounded-full p-0.5 ${bg} ${className}`}
+      className={`w-fit rounded-full p-0.5 ${bg} ${className}`}
     >
-      <div className="group rounded-full px-8 py-4 text-white uppercase hover:bg-white">
+      <div
+        className={`group rounded-full px-8 py-4 uppercase ${textColor} ${innerBg} ${innerHover}`}
+      >
         <p
           className={`font-heading flex items-center gap-x-2 text-xs font-semibold ${hover} sm:text-base`}
         >
