bab47e0a28c8f366d6709f9e885ea4db485090d8
Tamara Yustian <tamarayustian@gmail.com>
2026-07-06 21:14:12 +0800

    feat: add support to deploy on vercel

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
@@ -8,9 +7,13 @@ import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+const adapter = process.env.VERCEL
+  ? (await import('@astrojs/vercel')).default()
+  : (await import('@astrojs/node')).default({ mode: 'standalone' });
+
 export default defineConfig({
   output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  adapter,
   integrations: [icon(), react()],
   vite: {
     plugins: [tailwindcss()],
